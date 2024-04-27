0ba024e5d7271a0e9676667ae0bef12b968aba95
Isy Reyes <132601984+IsyReyes@users.noreply.github.com>
2024-04-27 14:52:23 -0600

    Implement celsius to farenheit conversion

--- obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae353809ae6cc72baf8e3a01705c092e7251de3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd5070ef81cb07953932d665c4f16befc45310f")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- variableexchange.cs
+++ variableexchange.cs
@@ -5,33 +5,31 @@ public class VariableExchange{
 
 
 
-public static void ExchangeVariable(){
+    public static void ExchangeVariable(){
 
-int variable1;
-int variable2;
-int exchangeContainer;
+    int variable1;
+    int variable2;
+    int exchangeContainer;
 
-Console.WriteLine("Vamos a intercambiar los valores de una variable a otra");
-Console.WriteLine("Ingresa el valor numérico entero de la primera variable:");
-variable1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ingresa el valor numérico entero de la segunda variable:");
-variable2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Vamos a intercambiar los valores de una variable a otra");
+    Console.WriteLine("Ingresa el valor numérico entero de la primera variable:");
+    variable1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingresa el valor numérico entero de la segunda variable:");
+    variable2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("En este momento la primera variable vale " + variable1 + " y la segunda variable vale " + variable2);
+    Console.WriteLine("En este momento la primera variable vale " + variable1 + " y la segunda variable vale " + variable2);
 
-Console.WriteLine("Generando intercambio de variables");
+    Console.WriteLine("Generando intercambio de variables");
 
-exchangeContainer = variable1;
+    exchangeContainer = variable1;
 
-variable1 = variable2;
-variable2 = exchangeContainer;
-
-
-Console.WriteLine("En este momento la primera variable vale " + variable1 + " y la segunda variable vale " + variable2);
-Console.WriteLine("Intercambio generado correctamente.");
+    variable1 = variable2;
+    variable2 = exchangeContainer;
 
 
+    Console.WriteLine("En este momento la primera variable vale " + variable1 + " y la segunda variable vale " + variable2);
+    Console.WriteLine("Intercambio generado correctamente.");
 
-}
+    }
 
 }
