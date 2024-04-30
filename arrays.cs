6aac485c131dba5d2633d1a3aa4ca8aafbbd2ea2
IsyReyes <isyreyes@hotmail.es>
2024-04-30 16:45:15 -0600

    started array methods

---
+++ arrays.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Arrays{
+
+    public static void ArrayEvensAndOdds(){
+
+        int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+        int[] evens = [];
+        int[] odds = [];
+
+        Console.WriteLine(numbers[1]);
+
+
+
+
+    }
+
+
+}
--- initializer.cs
+++ initializer.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 
 public class Initializer {
 //This class serves as a launcher for the program, initiating the switch manager. 
-  public static void Main(string[] args) {
+	public static void Main(string[] args) {
 
-    Console.WriteLine("Inicializando programa.");
+		Console.WriteLine("Inicializando programa.");
 
-    SayHi sayHi = new SayHi();
-    sayHi.Salute();
+		// SayHi sayHi = new SayHi();
+		// sayHi.Salute();
 
-  }
+		Arrays.ArrayEvensAndOdds();
+	}
 }
 
