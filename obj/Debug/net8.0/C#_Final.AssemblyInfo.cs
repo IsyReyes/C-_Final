a076f438f324db93eded3584cb055ed912f8b6a0
Isy Reyes <132601984+IsyReyes@users.noreply.github.com>
2024-05-02 00:57:33 -0600

    finished refactoring arrays, might include catch later

--- obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd61e1f81d0793846be3353eff4dfa23381dc3bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785a71f388cb6d7084f92b7731772c000020825e")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
