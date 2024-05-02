cd61e1f81d0793846be3353eff4dfa23381dc3bd
Isy Reyes <132601984+IsyReyes@users.noreply.github.com>
2024-05-02 00:05:37 -0600

    Implemented console tables for arrays

--- obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612c5e835c4e976b870e25a54dec0a5ee984d3e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87b45fc7de2d4b0fdb03caca9e8fbd1561e6283")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
