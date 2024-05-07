4d96b12f624dbf0f451ae268cb0699d76afaec55
IsyReyes <isyreyes@hotmail.es>
2024-05-07 13:29:47 -0600

    refactored method for failing or passig

--- obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae3d4012ceb9049c971799aeeb48afc502180b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29333387e242a6ae24742a6b7fa77e1a8488d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
