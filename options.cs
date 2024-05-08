7b093329d5e36deee5452ae3d4c995f5526584a7
Isy Reyes <132601984+IsyReyes@users.noreply.github.com>
2024-05-08 02:44:26 -0600

    refactored ExchangeVariable

--- options.cs
+++ options.cs
@@ -7,14 +7,14 @@ public class Options {
 	public static void printOptions(){
 
 		Console.WriteLine("¿Qué quieres hacer ahora mismo?");
-		Console.WriteLine("1. Suma de dos números.");
-		Console.WriteLine("2. Suma de tres números.");
-		Console.WriteLine("3. Multicálculo para 4 números.");
-		Console.WriteLine("4. Calcular la base de un triángulo.");
-		Console.WriteLine("5. Calcular cuántos segundos hay en X número de días.");
+		Console.WriteLine("1. Suma de dos números.");//done
+		Console.WriteLine("2. Suma de tres números.");//done
+		Console.WriteLine("3. Multicálculo para 4 números.");//done
+		Console.WriteLine("4. Calcular la base de un triángulo.");//done
+		Console.WriteLine("5. Calcular cuántos segundos hay en X número de días.");//done
 		Console.WriteLine("6. Intercambiar los valores de dos variables.");
 		Console.WriteLine("7. Buscar el ángulo faltante de un triángulo.");
-		Console.WriteLine("8. Convertir grados de Celsius a Farenheit.");
+		Console.WriteLine("8. Convertir grados de Celsius a Farenheit.");//done
 		Console.WriteLine("9. Operación con variables predefinidas, dale dinero a Juan.");
 		Console.WriteLine("10. Calcular tu edad hasta el año 2030");
 		Console.WriteLine("11. Realizar la ecuación cuadrática con coeficientes predefinidos.");
